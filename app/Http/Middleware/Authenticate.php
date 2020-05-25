f95c1980bbc19f8805d3043d596124d811b43589
Adam Mackintosh <agm1984@gmail.com>
2020-05-24 21:46:54 -0700

    upgrades Authenticate.php 'auth' middleware to better-support session expiry and refresh behaviour; the file now has some R&D and also litter/pollution in it, but this will be resolved soon after more unit tests are produced to ensure behaviour is as-intended

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,7 +4,12 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Support\Facades\Auth;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Auth;
 
 class Authenticate extends Middleware
 {
@@ -17,10 +22,59 @@ class Authenticate extends Middleware
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check()) {
-            return $next($request);
+        try {
+            \Log::debug('starting');
+            $user = JWTAuth::parseToken()->authenticate();
+
+            if (!$user) {
+                \Log::debug('1');
+                 return response()->json([
+                   'code'   => 101, // means auth error in the api,
+                   'response' => null // nothing to show
+                 ]);
+            }
+        } catch (TokenExpiredException $e) {
+            $refreshed = JWTAuth::refresh(JWTAuth::getToken());
+            $user = JWTAuth::setToken($refreshed)->toUser();
+            header('Authorization: Bearer ' . $refreshed);
+
+            // return response()->json([
+            //     'error' => 'token_expired',
+            // ], 401);
+        } catch (TokenInvalidException $e) {
+            return response()->json([
+                'error' => 'token_invalid',
+            ], 401);
+        } catch (JWTException $e) {
+            return response()->json([
+                'error' => 'token_absent',
+            ], 401);
+
         }
 
-        return response()->json(['error' => 'Unauthenticated.'], 401);
+        // catch (TokenExpiredException $e) {
+        //     // If the token is expired, then it will be refreshed and added to the headers
+        //     try {
+        //         $refreshed = JWTAuth::refresh(JWTAuth::getToken());
+        //         $user = JWTAuth::setToken($refreshed)->toUser();
+        //         header('Authorization: bearer ' . $refreshed);
+        //     } catch (JWTException $e) {
+        //          return response()->json([
+        //            'code'   => 103, // means not refreshable
+        //            'response' => null // nothing to show
+        //          ]);
+        //     }
+        // } catch (JWTException $e) {
+        //     \Log::debug($e);
+        //     return response()->json([
+        //            'code'   => 101, // means auth error in the api,
+        //            'response' => null // nothing to show
+        //     ]);
+        // }
+
+        // Login the user instance for global usage
+        Auth::login($user, false);
+
+        return  $next($request);
     }
 }
