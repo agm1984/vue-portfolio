6c7dbbbe0c4f0ece6e6ad78525768812fe6fc143
Adam Mackintosh <agm1984@gmail.com>
2020-06-01 14:51:37 -0700

    adds colour inversion utility, which will be used later to determine colour wheel opposites (ie: complementary colours) based on hex codes

--- resources/js/components/utils/invertColour.js
+++ resources/js/components/utils/invertColour.js
@@ -1,20 +1,21 @@
-function randomColor() {
+function randomColor() { // eslint-disable-line no-unused-vars
     let color;
     color = Math.floor(Math.random() * 0x1000000); // integer between 0x0 and 0xFFFFFF
     color = color.toString(16); // convert to hex
-    color = ("000000" + color).slice(-6); // pad with leading zeros
-    color = "#" + color; // prepend #
+    color = (`000000${color}`).slice(-6); // pad with leading zeros
+    color = `#${color}`; // prepend #
     return color;
 }
 
-function invertColor(hexTripletColor) {
+function invertColor(hexTripletColor) { // eslint-disable-line no-unused-vars
     let color = hexTripletColor;
     color = color.substring(1); // remove #
     color = parseInt(color, 16); // convert to integer
-    color = 0xFFFFFF ^ color; // invert three bytes
+    // eslint-disable-next-line no-bitwise
+    color ^= 0xFFFFFF; // invert three bytes
     color = color.toString(16); // convert to hex
-    color = ("000000" + color).slice(-6); // pad with leading zeros
-    color = "#" + color; // prepend #
+    color = (`000000${color}`).slice(-6); // pad with leading zeros
+    color = `#${color}`; // prepend #
     return color;
 }
 
@@ -24,7 +25,15 @@ function invertColor(hexTripletColor) {
  * @param {String} fromHex ex) #85e9b6
  */
 const invertColour = (fromHex) => {
-    return toHex;
+    let color = fromHex;
+    color = color.substring(1); // remove #
+    color = parseInt(color, 16); // convert to integer
+    // eslint-disable-next-line no-bitwise
+    color ^= 0xFFFFFF; // invert three bytes
+    color = color.toString(16); // convert to hex
+    color = (`000000${color}`).slice(-6); // pad with leading zeros
+    color = `#${color}`; // prepend #
+    return color;
 };
 
-export default invertColor;
+export default invertColour;
