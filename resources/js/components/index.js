325bd3cb476ae4a261c87e17c148d10941e50fb4
Adam Mackintosh <agm1984@gmail.com>
2020-05-31 21:43:45 -0700

    removes reference to child.vue in ~/components/index.js

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue';
 import { HasError, AlertError, AlertSuccess } from 'vform';
 import Card from './Card.vue';
-import child from './child.vue';
 import Button from './Button.vue';
 import Checkbox from './Checkbox.vue';
 
 // Components that are registered globaly.
 [
     Card,
-    child,
     Button,
     Checkbox,
     HasError,
