f64bb885883daf2657dc8654dfd3c2140ecf553a
Adam Mackintosh <agm1984@gmail.com>
2020-06-01 15:39:38 -0700

    changes a-button.vue to have square corners by default

--- resources/js/components/common/a-button.vue
+++ resources/js/components/common/a-button.vue
@@ -61,7 +61,7 @@ export default {
         rounded: {
             type: Boolean,
             required: false,
-            default: () => true,
+            default: () => false,
         },
 
         outlined: {
