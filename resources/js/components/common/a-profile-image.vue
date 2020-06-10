66ada8991faaa10dec3c1a14d8931fba4c6cd47e
Adam Mackintosh <agm1984@gmail.com>
2020-06-10 13:55:44 -0700

    adds beginnings of an a-profile-image component that will be responsible for rendering circular images with a sweet border around them

---
+++ resources/js/components/common/a-profile-image.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-64 h-64 p-8 border-2 rounded-full border-primary">
+        <!-- circular,  2px teal rounded border around it -->
+        <img class="bg-no-repeat bg-cover rounded-full" src="adam.png">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'a-profile-image',
+
+    data() {
+        return {};
+    },
+
+    computed: {},
+
+    methods: {},
+
+};
+</script>
