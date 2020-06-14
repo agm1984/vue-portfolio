37e8568ca91b9711a118720102b69ab2b8f671c4
Adam Mackintosh <agm1984@gmail.com>
2020-06-14 10:54:57 -0700

    adds user registration unit tests to match new auth system

--- tests/Auth/RegisterTest.php
+++ tests/Auth/RegisterTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Auth;
 
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-// use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Tests\TestCase;
 
@@ -12,119 +13,161 @@ class RegisterTest extends TestCase
 {
     use DatabaseTransactions;
 
+    protected $auth_guard = 'web';
+
     /** @test */
     public function it_can_register()
     {
-        $this->postJson(route('register'), [
+        $new_user = [
             'name' => 'Test Person',
             'email' => 'test@example.com',
             'password' => 'password',
+        ];
+
+        $this->postJson(route('register'), $new_user)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'user' => [
+                    'id' ,
+                    'status',
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at',
+                    'photo_url',
+                    'roles_list',
+                    'roles',
+                ],
+            ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => $new_user['name'],
+            'email' => $new_user['email'],
         ]);
-        // ->assertStatus(200)
-        // ->assertJsonStructure([
-        //     'user' => ['id', 'name', 'email', 'photo_url', 'roles_list', 'roles'],
-        //     'token',
-        //     'token_type',
-        //     'expires_in',
-        // ]);
+
+        $hashedPassword = User::query()->firstWhere('email', $new_user['email'])->password;
+        $this->assertTrue(Hash::check($new_user['password'], $hashedPassword), true);
+
+        $this->resetAuth();
     }
 
-    // /** @test */
-    // public function it_can_not_register_without_a_name()
-    // {
-    //     $this->postJson(route('register'), [
-    //         'name' => '',
-    //         'email' => 'test@example.com',
-    //         'password' => 'password',
-    //     ])
-    //     ->assertStatus(422)
-    //     ->assertJsonStructure(['message', 'errors' => ['name']]);
-    // }
+    /** @test */
+    public function it_should_get_two_cookies()
+    {
+        $new_user = [
+            'name' => 'Test Person',
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ];
 
-    // /** @test */
-    // public function it_can_not_register_with_a_name_too_long()
-    // {
-    //     $this->postJson(route('register'), [
-    //         'name' => Str::random(256),
-    //         'email' => 'test@example.com',
-    //         'password' => 'password',
-    //     ])
-    //     ->assertStatus(422)
-    //     ->assertJsonStructure(['message', 'errors' => ['name']]);
-    // }
+        $response = $this->postJson(route('register'), $new_user);
 
-    // /** @test */
-    // public function it_can_not_register_without_an_email()
-    // {
-    //     $this->postJson(route('register'), [
-    //         'name' => 'Test Person',
-    //         'email' => '',
-    //         'password' => 'password',
-    //     ])
-    //     ->assertStatus(422)
-    //     ->assertJsonStructure(['message', 'errors' => ['email']]);
-    // }
+        $response->assertCookieNotExpired(Str::slug(config('app.name'), '_').'_session');
+        $response->assertCookieNotExpired('XSRF-TOKEN');
+        $this->assertEquals(config('session.http_only'), true);
 
-    // /** @test */
-    // public function it_can_not_register_with_an_email_too_long()
-    // {
-    //     $this->postJson(route('register'), [
-    //         'name' => 'Test Person',
-    //         'email' => Str::random(256),
-    //         'password' => 'password',
-    //     ])
-    //     ->assertStatus(422)
-    //     ->assertJsonStructure(['message', 'errors' => ['email']]);
-    // }
+        $this->resetAuth();
+    }
 
-    // /** @test */
-    // public function it_can_not_register_with_an_existing_email()
-    // {
-    //     $this->postJson(route('register'), [
-    //         'name' => 'Test Person',
-    //         'email' => 'test@example.com',
-    //         'password' => 'password',
-    //     ])
-    //     ->assertStatus(200)
-    //     ->assertJsonStructure([
-    //         'user' => ['id' , 'name', 'email', 'photo_url', 'roles_list', 'roles'],
-    //         'token',
-    //         'token_type',
-    //         'expires_in',
-    //     ]);
 
-    //     Auth::logout();
+    /** @test */
+    public function it_should_throw_error_422_without_a_name()
+    {
+        $this->postJson(route('register'), [
+                'name' => '',
+                'email' => 'test@example.com',
+                'password' => 'password',
+            ])
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors' => ['name']]);
+    }
 
-    //     $this->postJson(route('register'), [
-    //         'name' => 'Test Person',
-    //         'email' => 'test@example.com',
-    //         'password' => 'password',
-    //     ])
-    //     ->assertStatus(422)
-    //     ->assertJsonStructure(['message', 'errors' => ['email']]);
-    // }
+    /** @test */
+    public function it_should_throw_error_422_with_a_name_too_long()
+    {
+        $this->postJson(route('register'), [
+                'name' => Str::random(256),
+                'email' => 'test@example.com',
+                'password' => 'password',
+            ])
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors' => ['name']]);
+    }
 
-    // /** @test */
-    // public function it_can_not_register_without_password()
-    // {
-    //     $this->postJson(route('register'), [
-    //         'name' => 'Test Person',
-    //         'email' => 'test@example.com',
-    //         'password' => '',
-    //     ])
-    //     ->assertStatus(422)
-    //     ->assertJsonStructure(['message', 'errors' => ['password']]);
-    // }
+    /** @test */
+    public function it_should_throw_error_422_without_an_email()
+    {
+        $this->postJson(route('register'), [
+                'name' => 'Test Person',
+                'email' => '',
+                'password' => 'password',
+            ])
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors' => ['email']]);
+    }
 
-    // /** @test */
-    // public function it_can_not_register_with_empty_form()
-    // {
-    //     $this->postJson(route('register'), [
-    //         'name' => '',
-    //         'email' => '',
-    //         'password' => '',
-    //     ])
-    //     ->assertStatus(422)
-    //     ->assertJsonStructure(['message', 'errors' => ['name', 'email', 'password']]);
-    // }
+    /** @test */
+    public function it_should_throw_error_422_with_an_email_too_long()
+    {
+        $this->postJson(route('register'), [
+                'name' => 'Test Person',
+                'email' => Str::random(256),
+                'password' => 'password',
+            ])
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors' => ['email']]);
+    }
+
+    /** @test */
+    public function it_should_throw_error_422_with_an_existing_email()
+    {
+        $dupe_user = [
+            'name' => 'Test Person',
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ];
+
+        $this->postJson(route('register'), $dupe_user);
+
+        $this->assertDatabaseHas('users', [
+            'name' => $dupe_user['name'],
+            'email' => $dupe_user['email'],
+        ]);
+
+        $this->resetAuth();
+
+        $this->postJson(route('register'), [
+                'name' => 'Test Person',
+                'email' => 'test@example.com',
+                'password' => 'password',
+            ])
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors' => ['email']]);
+
+        $this->assertGuest($this->auth_guard);
+    }
+
+    /** @test */
+    public function it_should_throw_error_422_without_password()
+    {
+        $this->postJson(route('register'), [
+                'name' => 'Test Person',
+                'email' => 'test@example.com',
+                'password' => '',
+            ])
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors' => ['password']]);
+    }
+
+    /** @test */
+    public function it_should_throw_error_422_with_empty_form()
+    {
+        $this->postJson(route('register'), [
+                'name' => '',
+                'email' => '',
+                'password' => '',
+            ])
+            ->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors' => ['name', 'email', 'password']]);
+    }
 }
