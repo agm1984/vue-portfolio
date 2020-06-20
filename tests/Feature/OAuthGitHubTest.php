0b8a2bfe2c89e3ebf78d2bc780449836d68fdca1
Adam Mackintosh <agm1984@gmail.com>
2020-06-19 18:08:38 -0700

    updates github and twitter oauth unit tests to work with cookie sessions, and to add safety regarding ->oauthProviders count

--- tests/Feature/OAuthGitHubTest.php
+++ tests/Feature/OAuthGitHubTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-// use Illuminate\Foundation\Testing\WithFaker;
+use App\OauthProvider;
 use App\User;
 use Illuminate\Support\Str;
 use Illuminate\Testing\TestResponse;
@@ -15,164 +15,179 @@ use Tests\TestCase;
 
 class OAuthGitHubTest extends TestCase
 {
-    // use DatabaseTransactions;
+    use DatabaseTransactions;
 
-    // public function setUp() : void
-    // {
-    //     parent::setUp();
+    public function setUp() : void
+    {
+        parent::setUp();
 
-    //     TestResponse::macro('assertText', function ($text) {
-    //         PHPUnit::assertTrue(Str::contains($this->getContent(), $text), "Expected text [{$text}] not found.");
+        TestResponse::macro('assertText', function ($text) {
+            PHPUnit::assertTrue(Str::contains($this->getContent(), $text), "Expected text [{$text}] not found.");
 
-    //         return $this;
-    //     });
+            return $this;
+        });
 
-    //     TestResponse::macro('assertTextMissing', function ($text) {
-    //         PHPUnit::assertFalse(Str::contains($this->getContent(), $text), "Expected missing text [{$text}] found.");
+        TestResponse::macro('assertTextMissing', function ($text) {
+            PHPUnit::assertFalse(Str::contains($this->getContent(), $text), "Expected missing text [{$text}] found.");
 
-    //         return $this;
-    //     });
-    // }
+            return $this;
+        });
+    }
 
-    // public function tearDown() : void
-    // {
-    //     parent::tearDown();
+    public function tearDown() : void
+    {
+        parent::tearDown();
 
-    //     m::close();
-    // }
+        m::close();
+    }
 
-    // /**
-    //  * Mocks the Socialite API that communicates externally.
-    //  *
-    //  * @param string $provider
-    //  * @param array $user
-    //  * @return void
-    //  */
-    // protected function mockSocialite(string $provider, ?array $user = null)
-    // {
-    //     $mock = Socialite::shouldReceive('stateless')
-    //         ->andReturn(m::self())
-    //         ->shouldReceive('driver')
-    //         ->with($provider)
-    //         ->andReturn(m::self());
+    /**
+     * Mocks the Socialite API that communicates externally.
+     *
+     * @param string $provider
+     * @param array $user
+     * @return void
+     */
+    protected function mockSocialite(string $provider, ?array $user = null)
+    {
+        $mock = Socialite::shouldReceive('stateless')
+            ->andReturn(m::self())
+            ->shouldReceive('driver')
+            ->with($provider)
+            ->andReturn(m::self());
 
-    //     if ($user) {
-    //         $mock->shouldReceive('user')
-    //             ->andReturn((new SocialiteUser)->setRaw($user)->map($user));
-    //     } else {
-    //         $mock->shouldReceive('redirect')
-    //             ->andReturn(redirect('https://url-to-provider'));
-    //     }
-    // }
+        if ($user) {
+            $mock->shouldReceive('user')
+                ->andReturn((new SocialiteUser)->setRaw($user)->map($user));
+        } else {
+            $mock->shouldReceive('redirect')
+                ->andReturn(redirect('https://url-to-provider'));
+        }
+    }
 
-    // /** @test */
-    // public function it_can_redirect_to_github()
-    // {
-    //     $this->mockSocialite('github');
+    /** @test */
+    public function it_redirects_to_github()
+    {
+        $this->mockSocialite('github');
 
-    //     $this->postJson(route('oauth.redirect', 'github'))
-    //         ->assertStatus(200)
-    //         ->assertJson(['url' => 'https://url-to-provider']);
-    // }
+        $this->postJson(route('oauth.redirect', 'github'))
+            ->assertStatus(200)
+            ->assertJson(['url' => 'https://url-to-provider']);
+    }
 
-    // /** @test */
-    // public function it_can_create_new_user_from_github_identity()
-    // {
-    //     $github_identity = [
-    //         'id' => '123',
-    //         'name' => 'New GitHub User',
-    //         'email' => 'new-github@example.com',
-    //         'token' => 'access-token',
-    //         'refreshToken' => 'refresh-token',
-    //     ];
+    /** @test */
+    public function it_can_create_new_user_from_github_identity()
+    {
+        $github_identity = [
+            'id' => '123',
+            'name' => 'New GitHub User',
+            'email' => 'new-github@example.com',
+            'token' => 'access-token',
+            'refreshToken' => 'refresh-token',
+        ];
 
-    //     $this->mockSocialite('github', $github_identity);
+        $this->mockSocialite('github', $github_identity);
 
-    //     $this->withoutExceptionHandling();
+        $this->withoutExceptionHandling();
 
-    //     $this->get(route('oauth.callback', 'github'))
-    //         ->assertText('token')
-    //         ->assertSuccessful();
+        $this->get(route('oauth.callback', 'github'))
+            ->assertText('user')
+            ->assertSuccessful();
 
-    //     $this->assertDatabaseHas('users', [
-    //         'name' => $github_identity['name'],
-    //         'email' => $github_identity['email'],
-    //     ]);
+        $this->assertDatabaseHas('users', [
+            'name' => $github_identity['name'],
+            'email' => $github_identity['email'],
+        ]);
 
-    //     $this->assertDatabaseHas('oauth_providers', [
-    //         'user_id' => User::query()->firstWhere('email', $github_identity['email'])->id,
-    //         'provider' => 'github',
-    //         'provider_user_id' => $github_identity['id'],
-    //         'access_token' => $github_identity['token'],
-    //         'refresh_token' => $github_identity['refreshToken'],
-    //     ]);
-    // }
+        $userId = User::query()->firstWhere('email', $github_identity['email'])->id;
 
-    // /** @test */
-    // public function it_can_update_github_identity_for_existing_user()
-    // {
-    //     $existing_user = $this->user();
+        $this->assertDatabaseHas('oauth_providers', [
+            'user_id' => $userId,
+            'provider' => 'github',
+            'provider_user_id' => $github_identity['id'],
+            'access_token' => $github_identity['token'],
+            'refresh_token' => $github_identity['refreshToken'],
+        ]);
 
-    //     $existing_user->oauthProviders()->create([
-    //         'provider' => 'github',
-    //         'provider_user_id' => '123',
-    //         'access_token' => 'access-token',
-    //         'refresh_token' => 'refresh-token',
-    //     ]);
+        $providerCount = OAuthProvider::query()->where('user_id', $userId)->where('provider', 'github')->count();
 
-    //     $updated_github_identity = [
-    //         'id' => '123',
-    //         'name' => 'Updated GitHub User',
-    //         'email' => $existing_user->email,
-    //         'token' => 'updated-access-token',
-    //         'refreshToken' => 'updated-refresh-token',
-    //     ];
+        $this->assertEquals($providerCount, 1);
+    }
 
-    //     $this->mockSocialite('github', $updated_github_identity);
+    /** @test */
+    public function it_can_update_github_identity_for_existing_user()
+    {
+        $existing_user = $this->user();
 
-    //     $this->withoutExceptionHandling();
+        $existing_user->oauthProviders()->updateOrCreate(['user_id' => $existing_user->id], [
+            'provider' => 'github',
+            'provider_user_id' => '123',
+            'access_token' => 'access-token',
+            'refresh_token' => 'refresh-token',
+        ]);
 
-    //     $this->get(route('oauth.callback', 'github'))
-    //         ->assertText('token')
-    //         ->assertSuccessful();
+        $updated_github_identity = [
+            'id' => '123',
+            'name' => 'Updated GitHub User',
+            'email' => $existing_user->email,
+            'token' => 'updated-access-token',
+            'refreshToken' => 'updated-refresh-token',
+        ];
 
-    //     $this->assertDatabaseHas('oauth_providers', [
-    //         'user_id' => $existing_user->id,
-    //         'access_token' => $updated_github_identity['token'],
-    //         'refresh_token' => $updated_github_identity['refreshToken'],
-    //     ]);
-    // }
+        $this->mockSocialite('github', $updated_github_identity);
 
-    // /** @test */
-    // public function it_can_add_github_identity_to_existing_user()
-    // {
-    //     $existing_user = $this->user();
+        $this->withoutExceptionHandling();
 
-    //     $this->assertTrue($existing_user->oauthProviders->count() === 0);
+        $this->get(route('oauth.callback', 'github'))
+            ->assertText('user')
+            ->assertSuccessful();
 
-    //     $github_identity = [
-    //         'id' => '123',
-    //         'name' => 'New GitHub User',
-    //         'email' => $existing_user->email,
-    //         'token' => 'access-token',
-    //         'refreshToken' => 'refresh-token',
-    //     ];
+        $this->assertDatabaseHas('oauth_providers', [
+            'user_id' => $existing_user->id,
+            'provider' => 'github',
+            'access_token' => $updated_github_identity['token'],
+            'refresh_token' => $updated_github_identity['refreshToken'],
+        ]);
 
-    //     $this->mockSocialite('github', $github_identity);
+        $providerCount = OAuthProvider::query()->where('user_id', $existing_user->id)->where('provider', 'github')->count();
 
-    //     $this->withoutExceptionHandling();
+        $this->assertEquals($providerCount, 1);
+    }
 
-    //     $this->get(route('oauth.callback', 'github'))
-    //         ->assertText('token')
-    //         ->assertSuccessful();
+    /** @test */
+    public function it_can_add_github_identity_to_existing_user()
+    {
+        $existing_user = $this->user();
 
-    //     $this->assertDatabaseHas('oauth_providers', [
-    //         'user_id' => User::query()->firstWhere('email', $github_identity['email'])->id,
-    //         'provider' => 'github',
-    //         'provider_user_id' => $github_identity['id'],
-    //         'access_token' => $github_identity['token'],
-    //         'refresh_token' => $github_identity['refreshToken'],
-    //     ]);
-    // }
+        $this->assertEquals($existing_user->oauthProviders->count(), 0);
+
+        $github_identity = [
+            'id' => '123',
+            'name' => 'New GitHub User',
+            'email' => $existing_user->email,
+            'token' => 'access-token',
+            'refreshToken' => 'refresh-token',
+        ];
+
+        $this->mockSocialite('github', $github_identity);
+
+        $this->withoutExceptionHandling();
+
+        $this->get(route('oauth.callback', 'github'))
+            ->assertText('user')
+            ->assertSuccessful();
+
+        $this->assertDatabaseHas('oauth_providers', [
+            'user_id' => $existing_user->id,
+            'provider' => 'github',
+            'provider_user_id' => $github_identity['id'],
+            'access_token' => $github_identity['token'],
+            'refresh_token' => $github_identity['refreshToken'],
+        ]);
+
+        $providerCount = OAuthProvider::query()->where('user_id', $existing_user->id)->where('provider', 'github')->count();
+
+        $this->assertEquals($providerCount, 1);
+    }
 
 }
