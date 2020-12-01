184df88ac8afd0f2b50555ad114e4ee3e8562cdc
Adam Mackintosh <agm1984@gmail.com>
2020-12-01 13:18:18 -0800

    fixes typo in sizing.vue

--- resources/js/pages/design/sizing.vue
+++ resources/js/pages/design/sizing.vue
@@ -3,12 +3,12 @@
 
         <a-heading level="1" dark>Space & Size</a-heading>
 
-        <a-pararaph>
+        <a-paragraph>
             This site uses primarily factors of 2, 4, 8, 16, 32, 64 for sizing. This relates loosely to the golden ratio 1.618~
             and to Fibonacci, in which the first few numbers are 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233. This is important
             because the first few numbers divisible by 16 are: 16, 32, 64, 96, 128, 256. Proportions tend to be
             pleasant when size dimensions are selected along this approximate range.
-        </a-pararaph>
+        </a-paragraph>
 
         <a-heading level="2" class="mt-32" dark>Sizing units</a-heading>
 
