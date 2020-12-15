d1d57670df324d2bdcf0f03490ca7a4a0e4d3515
Adam Mackintosh <agm1984@gmail.com>
2020-12-15 08:30:11 -0800

    adds linebreaks into code for home.vue

--- resources/js/pages/home/home.vue
+++ resources/js/pages/home/home.vue
@@ -3,10 +3,19 @@
 
         <div class="relative">
             <div class="bg-no-repeat bg-cover rounded-full h-96 w-96 border-1 border-primary" style="background-image: url('/adam.png')"></div>
-            <a-heading level="3" class="mt-64 mb-32" nunito>THE PORTFOLIO OF</a-heading>
-            <a-lambda class="absolute ml-8 mt-28" :fill-hex="secondary" :size="32"></a-lambda>
-            <h1 class="text-5xl font-aroly text-primary">Adam</h1>
-            <h1 class="text-5xl font-aroly text-primary">Mackintosh</h1>
+
+            <a-heading level="3" class="mt-64 mb-32" nunito>
+                THE PORTFOLIO OF
+            </a-heading>
+
+            <a-lambda class="absolute mt-0 -ml-0 lg:ml-8 lg:mt-28" :fill-hex="secondary" :size="32"></a-lambda>
+            <h1 class="text-2xl lg:text-5xl font-aroly text-primary">
+                Adam
+            </h1>
+
+            <h1 class="text-2xl lg:text-5xl font-aroly text-primary">
+                Mackintosh
+            </h1>
         </div>
 
     </div>
