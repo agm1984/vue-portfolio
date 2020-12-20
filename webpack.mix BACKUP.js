a78044a8ffb70ccd2c702f21435b3a2797a537fb
Adam Mackintosh <agm1984@gmail.com>
2020-12-19 16:36:20 -0800

    adds another hail mary attempt at prod styles

--- webpack.mix BACKUP.js
+++ webpack.mix BACKUP.js
@@ -5,16 +5,16 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 require('laravel-mix-bundle-analyzer');
 
-// function publishAssets() {
-//     const publicDir = path.resolve(__dirname, './public');
+function publishAssets() {
+    const publicDir = path.resolve(__dirname, './public');
 
-//     if (mix.inProduction()) {
-//         fs.removeSync(path.join(publicDir, 'dist'));
-//     }
+    if (mix.inProduction()) {
+        fs.removeSync(path.join(publicDir, 'dist'));
+    }
 
-//     fs.copySync(path.join(publicDir, 'build', 'dist'), path.join(publicDir, 'dist'));
-//     fs.removeSync(path.join(publicDir, 'build'));
-// }
+    fs.copySync(path.join(publicDir, 'build', 'dist'), path.join(publicDir, 'dist'));
+    fs.removeSync(path.join(publicDir, 'build'));
+}
 
 mix.js('resources/js/app.js', 'public/dist/js')
     .sass('resources/sass/app.scss', 'public/dist/css').options({
@@ -59,11 +59,11 @@ mix.webpackConfig({
     },
 });
 
-// mix.then(() => {
-//     if (!mix.config.hmr) {
-//         process.nextTick(() => publishAssets());
-//     }
-// });
+mix.then(() => {
+    if (!mix.config.hmr) {
+        process.nextTick(() => publishAssets());
+    }
+});
 
 // manually run analyzer at http://localhost:8888
 // mix.bundleAnalyzer({
