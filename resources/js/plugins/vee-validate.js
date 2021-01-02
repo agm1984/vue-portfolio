aecfb97e4c48f34eced2c63eff40f0d2e205aeb1
Adam Mackintosh <agm1984@gmail.com>
2021-01-01 16:39:09 -0800

    finishes vee-validate error messages

--- resources/js/plugins/vee-validate.js
+++ resources/js/plugins/vee-validate.js
@@ -23,50 +23,50 @@ import {
 
 extend('alpha_dash', {
     ...alpha_dash,
-    message: 'The {_field_} field can only include alphanumeric, dashes, and underscores',
+    message: 'This field can only include alphanumeric, dashes, and underscores',
 });
 
 extend('between', {
     ...between,
-    message: 'The {_field_} field must be between {min} and {max}',
+    message: 'This field must be between {min} and {max}',
 });
 
 extend('email', {
     ...email,
-    message: 'The {_field_} field must be a valid email',
+    message: 'This field must be a valid email',
 });
 
 extend('image', {
     ...image,
-    message: 'The {_field_} field must be an image',
+    message: 'This field must be an image',
 });
 
 extend('max', {
     ...max,
-    message: 'The {_field_} field is too long',
+    message: 'This field is too long',
 });
 
 extend('max_value', {
     ...max_value,
-    message: 'The {_field_} field is too large',
+    message: 'This field is too large',
 });
 
 extend('min', {
     ...min,
-    message: 'The {_field_} field is too short',
+    message: 'This field is too short',
 });
 
 extend('numeric', {
     ...numeric,
-    message: 'The {_field_} field must be a number',
+    message: 'This field must be a number',
 });
 
 extend('oneOf', {
     ...oneOf,
-    message: 'The {_field_} field has an invalid value',
+    message: 'This field has an invalid value',
 });
 
 extend('required', {
     ...required,
-    message: 'The {_field_} field is required',
+    message: 'This field is required',
 });
