d7e7c4730cdd9397d52dc38283f8803ff19b4a1d
Adam Mackintosh <adam@nevvon.com>
2025-11-07 22:09:32 -0800

    cleaned up some files

--- resources/js/pages/home/home.vue
+++ resources/js/pages/home/home.vue
@@ -7,19 +7,19 @@ useHead({
 </script>
 
 <template>
-    <div class="">
-
+    <div class="p-8">
         <div class="relative">
             <div
                 class="bg-no-repeat bg-cover rounded-full h-16 w-16 border"
                 style="background-image: url('/adam.png')"
             ></div>
 
-            <div class="mt-64 mb-32">
+            <div class="my-8">
                 THE PORTFOLIO OF
             </div>
 
-            <a-lambda class="absolute lg:ml-8 lg:mt-28" fill-hex="#fff000" :size="32"></a-lambda>
+            <a-lambda fill-hex="#000000" :size="32"></a-lambda>
+
             <div class="text-2xl lg:text-5xl font-aroly text-primary">
                 Adam
             </div>
@@ -28,6 +28,5 @@ useHead({
                 Mackintosh
             </div>
         </div>
-
     </div>
 </template>
