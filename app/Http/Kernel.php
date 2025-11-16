65cd9e58a0261b78b5ffcf3a327b248ed803f3a2
Adam Mackintosh <adam@nevvon.com>
2025-11-16 11:46:30 -0800

    updated Laravel to v11

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'transformTypes' => \App\Http\Middleware\TransformPayloadTypes::class,
