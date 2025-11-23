8889e3556c9bcdc97eafc0cd2d5f0c8cc5e11415
Adam Mackintosh <adam@nevvon.com>
2025-11-23 12:36:48 -0800

    removed unused files

--- resources/js/plugins/vue-meta.js
+++
@@ -1,9 +0,0 @@
-// /**
-//  * VueMeta provides helpers for mutating the Document head at run-time.
-//  *
-//  * @link https://vue-meta.nuxtjs.org/
-//  */
-// import Vue from 'vue';
-// import Meta from 'vue-meta';
-
-// Vue.use(Meta);
