b0c732536b82b521fa00c42416a28a7c0794cc16
Adam Mackintosh <adam@nevvon.com>
2025-11-23 12:28:46 -0800

    removed vue3-toastify

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,6 @@ import router from '~/router';
 import App from '~/components/App.vue';
 import '~/plugins';
 import 'primeicons/primeicons.css';
-import 'vue3-toastify/dist/index.css';
 
 const app = createApp(App);
 const head = createHead();
